ce3d491c30f2c8ec146fb20ffb5fff01d8e0adb3
sjagadee <sjagadee@hawk.iit.edu>
2017-01-27 18:01:58 -0600

    Adding new string problem

--- src/string_and_matrix/SprialOrderMatrixPrint.java
+++ src/string_and_matrix/SprialOrderMatrixPrint.java
@@ -18,25 +18,21 @@ public class SprialOrderMatrixPrint {
                     System.out.print(a[top][i] + " ");
                 }
                 top++;
-                dir = 1;
             }else if(dir == 1) {
                 for(int i =top; i <= bottom; i++) {
                     System.out.print(a[i][right] + " ");
                 }
                 right--;
-                dir = 2;
             } else if(dir == 2 ) {
                 for(int i = right; i >= left; i--) {
                     System.out.print(a[bottom][i] + " ");
                 }
                 bottom--;
-                dir = 3;
             } else  if(dir == 3) {
                 for(int i = bottom; i >= top; i--) {
                     System.out.print(a[i][left] + " ");
                 }
                 left++;
-                dir = 0;
             }
             dir = (dir + 1) % 4;
         }
---
+++ src/string_and_matrix/StringRotation.java
@@ -0,0 +1,34 @@
+package string_and_matrix;
+
+/**
+ * Created by srinivas on 1/27/17.
+ */
+public class StringRotation {
+
+    private static boolean isRotation(String str1, String str2) {
+        int length = str1.length();
+
+        if(length > 0 && length == str2.length()) {
+            String newStr = str1 + str2;
+            return isSubString(newStr, str2);
+        }
+        return false;
+    }
+
+    private static boolean isSubString(String str1, String str2) {
+        return str1.toLowerCase().contains(str2.toLowerCase());
+    }
+
+    public static void main(String[] args) {
+        String str1 = "Helloworld";
+        String str2 = "oworldhll";
+
+        boolean result = isRotation(str1, str2);
+
+        if(result) {
+            System.out.print(str2 + " is a rotation of " + str1);
+        } else {
+            System.out.print(str2 + " is not a rotation of " + str1);
+        }
+    }
+}
