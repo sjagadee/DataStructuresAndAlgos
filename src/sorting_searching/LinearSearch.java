279dcb9f7e5fbdae99c4e49a78df0a7033a995da
sjagadee <sjagadee@hawk.iit.edu>
2017-02-15 20:58:18 -0600

    Adding new code for searching

---
+++ src/sorting_searching/LinearSearch.java
@@ -0,0 +1,28 @@
+package sorting_searching;
+
+/**
+ * Created by srinivas on 2/15/17.
+ *
+ * Linear search
+ */
+public class LinearSearch {
+    public static int linearSearch(int[] array, int searchEle){
+        for (int i =0; i< array.length; i ++) {
+            if(array[i] == searchEle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 14, 5, 17, 8, 19, 2, 24, 26, 20, 9};
+        int index = linearSearch(array, 20);
+        if(index != -1) {
+            System.out.print("Value found at " + index);
+        } else {
+            System.out.print("Value not found");
+        }
+
+    }
+}
